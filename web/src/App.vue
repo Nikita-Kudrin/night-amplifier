a1a6c625c95c55977e522dc7544af2eac3200df5
Nikita Kudrin <niki.kudrin@gmail.com>
2026-08-14 16:18:22 +0300

    Fixing "About" icon

--- web/src/App.vue
+++ web/src/App.vue
@@ -158,7 +158,7 @@ onMounted(() => {
           style="margin-left: 0.25rem;"
           @click="showAbout = true"
       >
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <circle cx="12" cy="12" r="10"/>
           <path d="M12 16v-4"/>
           <path d="M12 8h.01"/>
